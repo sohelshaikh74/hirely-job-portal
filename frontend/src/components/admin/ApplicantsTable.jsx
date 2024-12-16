6374e48c0c2607117583e5aa0132caf8e5a5efe8
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-12-16 15:51:43 +0530

    updated

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -71,12 +71,12 @@ const ApplicantsTable = () => {
                     <span>NA</span>
                   )}
                 </TableCell>
-                {/* <TableCell>{item?.applicant.createdAt.split("T")[0]}</TableCell> */}
-                <TableCell>
+                <TableCell>{item?.createdAt.split("T")[0]}</TableCell>
+                {/* <TableCell>
                   {item?.applicant.createdAt
                     ? item.applicant.createdAt.split("T")[0]
                     : "N/A"}
-                </TableCell>
+                </TableCell> */}
 
                 <TableCell className="float-right cursor-pointer">
                   <Popover>
