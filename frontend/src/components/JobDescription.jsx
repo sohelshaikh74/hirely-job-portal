fe396a53372b99e4926d92b3c074e16e39f57c69
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-12-23 19:28:07 +0530

    Job description

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -169,6 +169,7 @@ import { setSingleJob } from "@/redux/jobSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 import { ArrowLeft } from "lucide-react";
+import Navbar from "./shared/Navbar";
 
 const JobDescription = () => {
   const { singleJob } = useSelector((store) => store.job);
@@ -231,93 +232,96 @@ const JobDescription = () => {
   const formattedRequirements = singleJob?.requirements?.join(", ");
 
   return (
-    <div className="max-w-7xl mx-auto my-10">
-      <div className="flex items-center justify-between">
-        <div>
-          <h1 className="font-bold text-xl">{singleJob?.title}</h1>
-          <div className="flex items-center gap-2 mt-4">
-            <Badge className={"text-blue-700 font-bold"} variant="ghost">
-              {singleJob?.position} Positions
-            </Badge>
-            <Badge className={"text-[#F83002] font-bold"} variant="ghost">
-              {singleJob?.jobType}
-            </Badge>
-            <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-              {singleJob?.salary}LPA
-            </Badge>
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto my-10">
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="font-bold text-xl">{singleJob?.title}</h1>
+            <div className="flex items-center gap-2 mt-4">
+              <Badge className={"text-blue-700 font-bold"} variant="ghost">
+                {singleJob?.position} Positions
+              </Badge>
+              <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+                {singleJob?.jobType}
+              </Badge>
+              <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
+                {singleJob?.salary}LPA
+              </Badge>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-3">
+            <Button
+              onClick={() => navigate("/jobs")}
+              variant="outline"
+              className="flex items-center gap-2 text-gray-500 font-semibold"
+            >
+              <ArrowLeft />
+              <span>Back</span>
+            </Button>
+            <Button
+              onClick={isApplied ? null : applyJobHandler}
+              disabled={isApplied}
+              className={`rounded-lg ${
+                isApplied
+                  ? "bg-gray-600 cursor-not-allowed"
+                  : "bg-[#7209b7] hover:bg-[#5f32ad]"
+              }`}
+            >
+              {isApplied ? "Already Applied" : "Apply Now"}
+            </Button>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-3">
-          <Button
-            onClick={() => navigate("/jobs")}
-            variant="outline"
-            className="flex items-center gap-2 text-gray-500 font-semibold"
-          >
-            <ArrowLeft />
-            <span>Back</span>
-          </Button>
-          <Button
-            onClick={isApplied ? null : applyJobHandler}
-            disabled={isApplied}
-            className={`rounded-lg ${
-              isApplied
-                ? "bg-gray-600 cursor-not-allowed"
-                : "bg-[#7209b7] hover:bg-[#5f32ad]"
-            }`}
-          >
-            {isApplied ? "Already Applied" : "Apply Now"}
-          </Button>
+        <h1 className="border-b-2 border-b-gray-300 font-medium py-4">
+          Job Description
+        </h1>
+        <div className="my-4">
+          <h1 className="font-bold my-1">
+            Role: <span className="pl-4 font-bold ">{singleJob?.title}</span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Location:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.location}
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Description:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.description}
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Requirements:
+            <span className="pl-4 font-bold">
+              {formattedRequirements || "No requirements listed for this job."}
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Experience:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.experienceLevel} yrs
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Salary:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.salary}LPA
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Total Applicants:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.applications?.length}
+            </span>
+          </h1>
+          <h1 className="font-bold my-1">
+            Posted Date:{" "}
+            <span className="pl-4 font-normal text-gray-800">
+              {singleJob?.createdAt.split("T")[0]}
+            </span>
+          </h1>
         </div>
-      </div>
-      <h1 className="border-b-2 border-b-gray-300 font-medium py-4">
-        Job Description
-      </h1>
-      <div className="my-4">
-        <h1 className="font-bold my-1">
-          Role: <span className="pl-4 font-bold ">{singleJob?.title}</span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Location:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.location}
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Description:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.description}
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Requirements:
-          <span className="pl-4 font-bold">
-            {formattedRequirements || "No requirements listed for this job."}
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Experience:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.experienceLevel} yrs
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Salary:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.salary}LPA
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Total Applicants:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.applications?.length}
-          </span>
-        </h1>
-        <h1 className="font-bold my-1">
-          Posted Date:{" "}
-          <span className="pl-4 font-normal text-gray-800">
-            {singleJob?.createdAt.split("T")[0]}
-          </span>
-        </h1>
       </div>
     </div>
   );
