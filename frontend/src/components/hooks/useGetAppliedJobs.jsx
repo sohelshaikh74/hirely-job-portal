32e476ff336225eb509537afca5da16a2470dfd6
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-12-25 17:34:59 +0530

    now updated

--- frontend/src/components/hooks/useGetAppliedJobs.jsx
+++ frontend/src/components/hooks/useGetAppliedJobs.jsx
@@ -1,3 +1,4 @@
+// import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
