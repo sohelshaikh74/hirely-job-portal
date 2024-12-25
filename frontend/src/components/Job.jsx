ec486414769d83ddd78a663d6814553e56616945
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-12-25 15:49:17 +0530

    , coma added

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -37,7 +37,7 @@ const Job = ({ job }) => {
         </Button>
         <div>
           <h1 className="font-medium text-lg">{job?.company?.name}</h1>
-          <p className="text-sm text-gray-500">{job?.location}India</p>
+          <p className="text-sm text-gray-500">{job?.location},India</p>
         </div>
       </div>
 
