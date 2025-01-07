47c617be01c79fe15871a2f5119a3ceb49d01e34
Sohel Shaikh <118370019+sohelshaikh74@users.noreply.github.com>
2025-01-07 21:13:28 +0530

    Update Navbar.jsx
    adding onclick on hirely text

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
     >
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
-          <h1 className="text-2xl font-bold">
+          <h1 className="text-2xl font-bold cursor-pointer" onClick={()=>navigate("/")}>
             Hire<span className="text-[#f83002]">ly</span>
           </h1>
         </div>
